65662db9ee502a67581838e780cf23511468fbdb
Mrutyunjay01 <mrutyunjay.tutulu1021@gmail.com>
2020-05-07 21:20:41 +0530

    workplace cleaned

--- Hackerrank_practice/find_the_runner_up_score.py
+++
@@ -1,6 +0,0 @@
-i = int(input())
-list1 = list(map(int, input().strip().split()))[:i]
-max1 = max(list1)
-while max(list1) == max1:
-    list1.remove(max(list1))
-print(max(list1))
--- Interview/swap.py
+++
@@ -1,6 +0,0 @@
-def swap(x, y):
-    x = x * y
-    y = x // y
-    x = x // y
-    return x, y
-print(swap(2, 3))
